3784af48173c631346780ecf5cc0b4aa04220423
Hush-Lee <liyu981203@163.com>
2025-01-01 21:40:20 +0800

    init

--- BroadCast/lib/renew.sh
+++ BroadCast/lib/renew.sh
@@ -1,20 +1,12 @@
 file=$(find . -name '*.cpp' -print)
-nu=""
-if [ $file != $nu ]
-then
-	for item in $file
-	do
-		name=$(echo $item|sed 's/.\///g'|sed 's/.cpp//g')
-		cat>CMakeLists.txt<<EOF
-	add_library($name
-		STATIC 
-		$name.hpp
-		$name.cpp
-	)
-EOF
-	done
-else
+for item in $file
+do
+	name=$(echo $item|sed 's/.\///g'|sed 's/.cpp//g')
 	cat>CMakeLists.txt<<EOF
+add_library($name
+	STATIC 
+	$name.hpp
+	$name.cpp
+)
 EOF
-fi
-
+done
