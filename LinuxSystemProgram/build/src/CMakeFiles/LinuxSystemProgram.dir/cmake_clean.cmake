b7c539436aa0511990e64c80d7b3e973769f1c88
Hush-Lee <liyu981203@163.com>
2025-01-04 23:02:45 +0800

    day4

---
+++ LinuxSystemProgram/build/src/CMakeFiles/LinuxSystemProgram.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/LinuxSystemProgram.dir/main.cpp.o"
+  "CMakeFiles/LinuxSystemProgram.dir/main.cpp.o.d"
+  "LinuxSystemProgram"
+  "LinuxSystemProgram.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/LinuxSystemProgram.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
