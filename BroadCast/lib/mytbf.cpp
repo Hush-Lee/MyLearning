2fea1930fd89b0cefdca0ca7e2ee4ac7e3780879
Hush-Lee <liyu981203@163.com>
2025-01-27 23:11:43 +0800

    day27

--- BroadCast/lib/mytbf.cpp
+++ BroadCast/lib/mytbf.cpp
@@ -9,6 +9,10 @@
 #include <cstring>
 #include <pthread.h>
 
+inline int min(int a,int b){
+	return a>b?b:a;
+}
+
 struct mytbf_st{
 	int cps;
 	int brust;
@@ -51,4 +55,45 @@ mytbf_t *mytbf_init(int cps,int brust){
 	job[me->pos]=me;
 	pthread_mutex_unlock(&mut_job);
 	return me;
+}
+
+int mytbf_fetchtocken(mytbf_t* ptr,int size){
+	struct mytbf_st *me=ptr;
+	pthread_mutex_lock(&me->mut);
+	while(me->token<=0){
+		pthread_cond_wait(&me->cond,&me->mut);
+	}
+	int n=min(me->token,size);
+	me->token-=n;
+	pthread_mutex_unlock(&me->mut);
+	return 0;
+}
+
+int mytbf_returntoken(mytbf_t *ptr, int size){
+	struct mytbf_st *me = ptr;
+
+	pthread_mutex_lock(&me->mut);
+
+	me->token+=size;
+	if(me->token>me->brust){
+		me->token= me->brust;
+	}
+
+	pthread_cond_broadcast(&me->cond);
+	pthread_mutex_unlock(&me->mut);
+
+	return 0;
+}
+
+
+int mytbf_destory(mytbf_t *ptr){
+	struct mytbf_st * me = ptr;
+
+	pthread_mutex_lock(&mut_job);
+	job[me->pos]=nullptr;
+	pthread_mutex_unlock(&mut_job);
+	pthread_mutex_destroy(&me->mut);
+	pthread_cond_destroy(&me->cond);
+	free(ptr);
+	return 0;
 }
