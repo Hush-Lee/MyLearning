a8f87fa2755b7a12347e2a7d0cb6ed3d4c0ea0bf
Hush-Lee <liyu981203@163.com>
2025-02-17 22:03:58 +0800

    day48

--- muduo/lib/eventLoop.cpp
+++ muduo/lib/eventLoop.cpp
@@ -1,32 +1,48 @@
 #include "eventLoop.hpp"
+#include "common.hpp"
 #include <cassert>
 #include <poll.h>
-__thread EventLoop* t_loopInThisThread =0;
+__thread muduo::EventLoop* t_loopInThisThread =0;
 //EventLoop由IO线程创建,由这个线程运行loop(),生命周期和线程一样,不需要额外关注;
-EventLoop::EventLoop():looping_(false),threadID_(std::this_thread::get_id()){
-	LOG_TRACE<<"EventLoop created"<<this<<"in thread "<< threadID_;
+muduo::EventLoop::EventLoop():looping_(false),threadId_(std::this_thread::get_id()){
+	LOG_TRACE<<"EventLoop created"<<this<<"in thread "<< threadId_;
 	if(t_loopInThisThread){
-		LOG_FATAL<<"Another EventLoop"<<t_loopInThisThread<<" exists in this thread "<<threadID_;
+		LOG_FATAL<<"Another EventLoop"<<t_loopInThisThread<<" exists in this thread "<<threadId_;
 	}else {
 		t_loopInThisThread = this;
 	}
 }
 
-EventLoop::~EventLoop(){
+muduo::EventLoop::~EventLoop(){
 	assert(!looping_);
 	t_loopInThisThread=nullptr;
 }
 
-EventLoop* EventLoop::getEventLoopOfCurrentThread(){
+muduo::EventLoop* muduo::EventLoop::getEventLoopOfCurrentThread(){
 	return t_loopInThisThread;
 }
 
-void EventLoop::loop(){
+void muduo::EventLoop::loop(){
 	assert(!looping_);
 	assertInLoopThread();
 	looping_=true;
-	::poll(nullptr,0,5*1000);
+	quit_=false;
+	while(!quit_){
+		activeChannels_.clear();
+		poller_->poll(kPollTimeMs,&activeChannels_);
+		for(auto it=activeChannels_.begin();it!=activeChannels_.end();++it){
+			(*it)->handleEvent();
+		}
+	}
 	LOG_TRACE<<"EventLoop "<<this<<" stop looping";
 	looping_=false;
 }
+void  muduo::EventLoop::quit(){
+	quit_=true;
+}
 
+void muduo::EventLoop::updateChannel(muduo::Channel * channel){
+	assert(channel->ownerLoop() == this);
+	assertInLoopThread();
+	poller_->updateChannel(channel);
+}
--- muduo/lib/eventLoop.hpp
+++ muduo/lib/eventLoop.hpp
@@ -1,12 +1,13 @@
 #pragma once
-#include "channel.hpp"
 #include"common.hpp"
 #include <atomic>
 #include <memory>
 #include <thread>
+#include <vector>
 namespace muduo {
 	class EventLoop:noncopyable{
 	public:
+		typedef std::vector<Channel*> ChannelList;
 		EventLoop();
 		~EventLoop();
 		void loop();
@@ -15,18 +16,18 @@ namespace muduo {
 				abortNotInLoopThread();
 			}
 		}
-		bool isINLoopThread()const{return threadID_==std::this_thread::get_id();}
+		bool isINLoopThread()const{return threadId_==std::this_thread::get_id();}
 		static EventLoop* getEventLoopOfCurrentThread();
 		void updateChannel(Channel* ch);
-
+		void quit();
 	private:
 		void abortNotInLoopThread();
 
 		std::atomic_bool looping_;
 		const std::thread::id threadId_;
 		std::unique_ptr<Poller> poller_;
-		std::atomic_bool quit;
-
+		std::atomic_bool quit_;
+		ChannelList activeChannels_;
 
 	};
 }
