d79030cf66404a00ffed06dbe19adaf8ed249b4e
Hush-Lee <liyu981203@163.com>
2025-03-10 23:32:34 +0800

    day67

--- myMuduo/lib/Buffer.cpp
+++ myMuduo/lib/Buffer.cpp
@@ -1,4 +1,5 @@
 #include "Buffer.hpp"
+#include <cassert>
 #include <cerrno>
 #include <cstddef>
 #include <iterator>
@@ -7,7 +8,7 @@
 ssize_t Buffer::readFd(int fd,int* savedErr){
 	char extrabuf[65535];
 	struct iovec vec[2];
-	const size_t writable=writableByess();
+	const size_t writable=writableBytes();
 	vec[0].iov_base =beginWrite();
 	vec[0].iov_len = writable;
 	vec[1].iov_base = extrabuf;
@@ -24,9 +25,6 @@ ssize_t Buffer::readFd(int fd,int* savedErr){
 	return n;
 }
 
-ssize_t Buffer::writeFd(int fd,int* savedErr){
-
-}
 
 void Buffer::retrieve(size_t len){
 	if(len<readableBytes()){
@@ -36,24 +34,27 @@ void Buffer::retrieve(size_t len){
 	}
 }
 
-void Buffer::retrieveAll(){
-	writeIndex_=kCheapPrepend;
-	readIndex_=kCheapPrepend;
-}
-
-char* Buffer::beginWrite(){
-	return buffer_.data()+writeIndex_;
-}
-
-char* Buffer::peek(){
-	return buffer_.data()+readIndex_;
-}
 
 
-size_t Buffer::writableByess()const{
-	return buffer_.size()-writeIndex_;
-}
+void Buffer::makeSpace(size_t len)
+  {
+    if (writableBytes() + prependableBytes() < len + kCheapPrepend)
+    {
+      // FIXME: move readable data
+      buffer_.resize(writeIndex_+len);
+    }
+    else
+    {
+      // move readable data to the front, make space inside buffer
+      assert(kCheapPrepend < readIndex_);
+      size_t readable = readableBytes();
+      std::copy(begin()+readIndex_,
+                begin()+writeIndex_,
+                begin()+kCheapPrepend);
+      readIndex_ = kCheapPrepend;
+      writeIndex_ = readIndex_ + readable;
+      assert(readable == readableBytes());
+    }
+  }
 
-size_t Buffer::readableBytes()const{
-	return writeIndex_-readIndex_;
-}
+
--- myMuduo/lib/Buffer.hpp
+++ myMuduo/lib/Buffer.hpp
@@ -1,32 +1,114 @@
 #pragma once
+#include <algorithm>
+#include <cassert>
+#include <cstddef>
 #include <stdlib.h>
+#include <sys/types.h>
+#include <utility>
 #include <vector>
+#include <string>
 
 #define BUFSIZE 8092
 
 class Buffer{
 public:
+	explicit Buffer(size_t initialSize=kInitialiSize)
+		:buffer_(kCheapPrepend+initialSize),
+		writeIndex_(kCheapPrepend),
+		readIndex_(kCheapPrepend){
+		assert(writableBytes()==initialSize);
+		assert(readableBytes()==0);
+		assert(prependableBytes()==kCheapPrepend);
+		}
 	static const int kCheapPrepend=8;
 	static const size_t kInitialiSize=1024;
 
+	void swap(Buffer &rhs){
+		buffer_.swap(rhs.buffer_);
+		std::swap(this->writeIndex_,rhs.writeIndex_);
+		std::swap(this->readIndex_,rhs.readIndex_);
+	}
 	ssize_t readFd(int fd,int *savedErr);
-	ssize_t writeFd(int fd,int *savedErr);
-	char* peek();
-	size_t readableBytes()const;
+	const char* peek()const{
+		return begin()+readIndex_;
+	}
+	size_t readableBytes()const{
+		return writeIndex_-readIndex_;
+	}
 	void retrieve(size_t len);
-	void retrieveAll();
-	std::string retrieveAllAsString();
+	void retrieveAll(){
+		writeIndex_=kCheapPrepend;
+		readIndex_=kCheapPrepend;
+	}
 
-	char*beginWrite();
-	size_t writableByess()const;
-	void hasWriten(size_t len);
+	std::string retrieveAsString(size_t len){
+		assert(len<=readableBytes());
+		std::string res(peek(),len);
+		retrieve(len);
+		return res;
+	}
+	std::string retrieveAllAsString(){
+		return retrieveAsString(readableBytes());
+	}
 
-	void append(const char* data,size_t lean);
-	void append(const std::string&str);
+	char* beginWrite(){
+		return begin()+writeIndex_;
+	}
+	const char* beginWrite()const{
+		return begin()+writeIndex_;
+	}
+	size_t writableBytes()const{
+		return buffer_.size()-writeIndex_;
+	}
+	size_t prependableBytes()const{
+		return readIndex_;
+	}
+	void hasWriten(size_t len){
+		assert(len<=writableBytes());
+		writeIndex_+=len;
+	}
+
+	const char* findCRLF()const{
+		const char*crlf=std::search(peek(),beginWrite(),kCRLF,kCRLF+2);
+		return crlf==beginWrite()?nullptr:crlf;
+	}
+
+	const char* findCRLF(char * start)const{
+		assert(start>=peek());
+		assert(start<=beginWrite());
+		const char*crlf=std::search(peek(),beginWrite(),kCRLF,kCRLF+2);
+		return crlf==beginWrite()?nullptr:crlf;
+	}
+
+	void append(const char* data,size_t len){
+		ensureWritableBytes(len);
+		std::copy(data,data+len,beginWrite());
+	}
+	void append(const std::string&str){
+		append(static_cast<char *>(str),str.size());
+	}
 	
+	void makeSpace(size_t len);
+
+
+	void ensureWritableBytes(size_t len){
+		if(len>writableBytes()){
+			makeSpace(len);
+		}
+		assert(len<=writableBytes());
+	}
+	char * begin(){
+		return buffer_.begin().base();
+	}
+	const char* begin()const{
+		return buffer_.begin().base();
+	}
 
 private:
+
 	std::vector<char> buffer_; 
 	size_t readIndex_;//从这里开始读消息
 	size_t writeIndex_;//从这里开始写消息,也就是可读消息到此为止
+	
+	static const char kCRLF[];
 };
