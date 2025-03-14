e7246b9df4d75537edcca836e51d16165bc8fa3d
Hush-Lee <liyu981203@163.com>
2025-03-14 23:46:45 +0800

    day70

--- myMuduo/lib/LogStream.hpp
+++ myMuduo/lib/LogStream.hpp
@@ -5,6 +5,16 @@
 const int kSmallBuffer=4000;
 const int kLargerBuffer=4000*1000;
 
+template<int SIZE>
 class FixedBuffer:noncopyable{
 
+private:
+	const char* end()const{return data_+sizeof(data_);}
+	
+	static void cookieStart();
+	static void cookieEnd();
+
+	void(*cookie_)();
+	char data_[SIZE];
+	char*cur_;
 };
