2141d035f2d2c0dbf11d6d5b812a18fcec96cad8
Hush-Lee <liyu981203@163.com>
2025-03-15 23:48:17 +0800

    day72

--- myMuduo/lib/LogStream.hpp
+++ myMuduo/lib/LogStream.hpp
@@ -7,7 +7,23 @@ const int kLargerBuffer=4000*1000;
 
 template<int SIZE>
 class FixedBuffer:noncopyable{
+	FixedBuffer():cur_(data_){
+		setCookie(cookieStart);
+	}
+	~FixedBuffer(){
+		setCookie(cookieEnd);
+	}
 
+	void bzero(){memZero(data_,sizeof(data_));}
+	void add(size_t len){
+		cur_+=len;
+	}
+	int length()const{return static_cast<int>(cur_-data_);}
+	void setCookie(void (*cookie)()){
+		cookie_=cookie;
+	}
+	std::string toString()const{return std::string(data_,length());}
+	std::string_view toString_view()const{return std::string_view(data_,length());}
 private:
 	const char* end()const{return data_+sizeof(data_);}
 	
