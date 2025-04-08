48ae6875853d22560fa34e7bf092c952f680a391
Hush-Lee <liyu981203@163.com>
2025-04-08 22:12:46 +0800

    day99

--- myMuduo/lib/EpollPoller.cpp
+++ myMuduo/lib/EpollPoller.cpp
@@ -3,9 +3,12 @@
 #include "Poller.hpp"
 #include "Logging.hpp"
 #include "Timestamp.hpp"
+#include "Channel.hpp"
+#include <cassert>
 #include <cerrno>
 #include <cstddef>
 #include <poll.h>
+#include <random>
 #include <sys/epoll.h>
 #include <sys/poll.h>
 #include <unistd.h>
@@ -19,7 +22,7 @@ static_assert(EPOLLHUP == POLLHUP, "epoll use same flag value as poll");
 
 
 namespace {
-const int kNew=1;
+const int kNew=-1;
 const int kAdded=1;
 const int kDeleted=2;
 }
@@ -64,5 +67,98 @@ Timestamp EpollPoller::poll(int timeoutMs,ChannelList*activeChannels){
 
 }
 
-void EpollPoller::fillActiveChannels()
+void EpollPoller::fillActiveChannels(int numEvents,ChannelList*activeChannels)const{
+	assert(implicit_cast<size_t>(numEvents)<=events_.size());
+	for(int i=0;i<numEvents;++i){
+		Channel* channel =static_cast<Channel*>(events_[i].data.ptr);
+#ifndef NODEBUG
+		int fd=channel->fd();
+		ChannelMap::const_iterator it=channels_.find(fd);
+		assert(it!=channels_.end());
+		assert(it->second==channel);
+#endif
+		channel->set_revents(events_[i].events);
+		activeChannels->push_back(channel);
 
+	}
+	
+}
+
+void EpollPoller::updateChannel(Channel*channel){
+	Poller::assertInLoopThread();
+	const int index=channel->index();
+	LOG_TRACE<<"fd= "<<channel->fd()<<" events = "<<channel->events()<<" index "<<index;
+	if(index==kNew||index==kDeleted){
+		int fd=channel->fd();
+		if(index==kNew){
+			assert(channels_.find(fd)==channels_.end());
+			channels_[fd]=channel;
+		}else{
+			assert(channels_.find(fd)!=channels_.end());
+			assert(channels_[fd]==channel);
+		}
+		channel->set_index(kAdded);
+		update(EPOLL_CTL_ADD, channel);
+	}else{
+		int fd=channel->fd();
+		(void)fd;
+		assert(channels_.find(fd)!=channels_.end());
+		assert(channels_[fd]==channel);
+		assert(index==kAdded);
+		if(channel->isNoneEvent()){
+			update(EPOLL_CTL_DEL,channel);
+			channel->set_index(kDeleted);
+		}else{
+			update(EPOLL_CTL_MOD,channel);
+		}
+	}
+}
+
+void EpollPoller::removeChannel(Channel* channel){
+	Poller::assertInLoopThread();
+	int fd=channel->fd();
+	LOG_TRACE<<"fd= "<<fd;
+	assert(channels_.find(fd)!=channels_.end());
+	assert(channels_[fd]==channel);
+	assert(channel->isNoneEvent());
+	int index=channel->index();
+	assert(index==kAdded||index==kDeleted);
+	size_t n=channels_.erase(fd);
+	(void)n;
+	assert(n==1);
+	if(index==kAdded){
+		update(EPOLL_CTL_DEL,channel);
+	}
+	channel->set_index(kNew);
+}
+
+
+void EpollPoller::update(int operation,Channel* channel){
+	struct epoll_event event;
+	memZero(&event, sizeof(event));
+	event.events=channel->events();
+	event.data.ptr=channel;
+	int fd=channel->fd();
+	LOG_TRACE<<"epoll_ctl op"<<operationToString(operation)<<" fd "<<fd<<" event = {"<<channel->eventsToString()<<"} ";
+	if(::epoll_ctl(epollfd_, operation, fd,&event)<0){
+		if(operation==EPOLL_CTL_DEL){
+			LOG_SYSERR<<"epoll_ctl  op="<<operationToString(operation)<<" fd = "<<fd;
+		}else{
+			LOG_SYSFATAL<<"epoll_ctl  op="<<operationToString(operation)<<" fd = "<<fd;
+		}
+	}
+}
+
+const char* EpollPoller::operationToString(int op){
+	switch (op) {
+		case EPOLL_CTL_ADD:
+			return "ADD";
+		case EPOLL_CTL_DEL:
+			return "DEL";
+		case EPOLL_CTL_MOD:
+			return "MOD";
+		default:
+			assert(false&&"ERROR op");
+			return "Unknown operation";
+	}
+}
