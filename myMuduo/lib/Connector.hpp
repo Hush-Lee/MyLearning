d75daeaf535108c09910e0d3b701e80b032a75df
Hush-Lee <liyu981203@163.com>
2025-04-09 23:25:48 +0800

    day100

---
+++ myMuduo/lib/Connector.hpp
@@ -0,0 +1,44 @@
+#pragma once
+#include "Classes.hpp"
+#include <memory>
+#include <functional>
+
+class Channel;
+class InetAddress;
+class EventLoop;
+class Connector:noncopyable,public std::enable_shared_from_this<Connector>{
+public:
+	using NewConnectionCallback=std::function<void(int sockfd)>;
+	Connector(EventLoop*loop,const InetAddress&serverAddr);
+	~Connector();
+	void setNewConnectionCallback(NewConnectionCallback cb){
+		newConnectionCallback_=std::move(cb);
+	}
+	void start();
+	void restart();
+	void stop();
+	const InetAddress& serverAddress()const{return serverAddr_;}
+private:
+	enum States{kDisConnected,kConnecting,kConnected};
+	static const int kMaxRetryDelayMs=30*1000;
+	static const int kInitRetryDelayMs=500;
+	void setState(States s){state_=s;}
+	void startInLoop();
+	void stopInLoop();
+	void connect();
+	void connecting(int sockfd);
+	void handleWrite();
+	void handleErroe();
+	void retry(int sockfd);
+	int removeAndResetChannel();
+	void resetChannel();
+
+	EventLoop *loop_;
+	InetAddress serverAddr_;
+	bool connect_;
+	States state_;
+	std::unique_ptr<Channel>channel_;
+	NewConnectionCallback newConnectionCallback_;
+	int retryDelayMs_;
+
+};
