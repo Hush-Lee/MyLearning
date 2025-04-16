c7be32ea3e2498c410d3ed791223b9caaab42aa1
Hush-Lee <liyu981203@163.com>
2025-04-16 21:23:13 +0800

    day106

--- myMuduo/lib/EventLoopThread.cpp
+++ myMuduo/lib/EventLoopThread.cpp
@@ -22,5 +22,26 @@ EventLoopThread::~EventLoopThread(){
 }
 EventLoop* EventLoopThread::startLoop(){
 	assert(!thread_.started());
+	thread_.start();
+	EventLoop*loop=nullptr;
+	std::unique_lock<std::mutex>ul(mutex_);
+	cond_.wait(ul,loop==nullptr);
+	loop_=loop;
+	return loop;
 }
 
+
+void EventLoopThread::threadFunc(){
+	EventLoop loop;
+	if(callback_){
+		callback_(&loop);
+	}
+	{
+		std::lock_guard<std::mutex>lg(mutex_);
+		loop_=&loop;
+		cond_.notify_all();
+	}
+	loop.loop();
+	std::lock_guard<std::mutex>lg(mutex_);
+	loop=nullptr;
+}
